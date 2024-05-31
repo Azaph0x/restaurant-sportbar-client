a2ff83c60f3ea90eb1d81a19222db4e8823c3bf1
João Gabriel <azaph.contato@gmail.com>
2024-05-31 09:21:14 -0300

    correção para evitar erros de sessão

--- src/app/pages/mesa/mesa.component.ts
+++ src/app/pages/mesa/mesa.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { ModalController, NavController } from '@ionic/angular';
 import { Observable, Subject, takeUntil, tap } from 'rxjs';
-import { Mesa, Produto } from 'src/app/models/mesa';
+import { ClientDataModalComponent } from 'src/app/components/client-data-modal/client-data-modal.component';
+import { Mesa, Produto, mesaStatus } from 'src/app/models/mesa';
 import { MesaService } from 'src/app/services/mesa/mesa.service';
 import { ProdutoService } from 'src/app/services/produto/produto.service';
 
@@ -15,29 +17,50 @@ export class MesaComponent  implements OnInit, OnDestroy {
   mesaInUse!: Mesa;
 
   pedidos: Produto[] = [];
+  id!: number;
 
   constructor(
     private mesaService: MesaService,
     private navController: NavController,
-    private produtoService: ProdutoService
+    private produtoService: ProdutoService,
+    private activedRoute: ActivatedRoute,
+    private modalController: ModalController
   ) { }
 
   subject: Subject<any> = new Subject();
   produtos$!: Observable<Produto[]>;
 
   ngOnInit() {
-   this.mesaService.getMesa().pipe(
-      takeUntil(this.subject),
-      tap((r) => {
-        if(r == null) {
-          this.navController.navigateForward(['']);
-          return;
-        }
-        this.mesaInUse = r;
-      })
-    ).subscribe();
-    this.produtos$ = this.produtoService.getProdutos().pipe(takeUntil(this.subject));
+    const params = this.activedRoute.snapshot.params;
+    if(params['id']) {
+      this.id = params['id'];
+
+    } else {
+      this.navController.navigateForward(['']);
+      return;
+    }
+;    this.produtos$ = this.produtoService.getProdutos().pipe(takeUntil(this.subject));
   }
+
+  async openConfigClient(mesa: mesaStatus) {
+    const modal = await this.modalController.create({
+       component: ClientDataModalComponent,
+       mode: 'ios',
+       initialBreakpoint: 0.8
+     })
+     modal.present()
+     await modal.onDidDismiss().then((r) => {
+       if(r.data) {
+          this.mesaService.useMesa({
+            id: this.id,
+            ativa: true,
+            nome: r.data.nome
+          })
+
+          this.navController.navigateForward(['mesa']);
+       }
+     })
+   }
 
   addValue(pedido: Produto) {
     this.pedidos.push(pedido);
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path: 'mesa',
+    path: 'mesa/:id',
     component: MesaComponent
   },
   {
